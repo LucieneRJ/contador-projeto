ce36c4318483534f2adeb692af1ea6e8716c7035
Luciene Freitas <luciene.freitas2015sjm@gmail.com>
2022-04-29 07:11:01 -0300

    Alteração
    realizada alteração, pois no JS apresentava erro na condicional

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
-//criar variavel inicio,fim e passo
 function contar() {
-    var iniciotxt = window.document.getElementById('inicio')
-    var fimtxt = window.document.getElementById('fim')
-    var passotxt = window.document.getElementById('passo')
+    .document.getElementById('txtini')
+    var formFim = window.document.getElementById('txtfim')
+    var formPas = window.document.getElementById('txtpas')
+    var res = window.document.getElementById('res')
 
-    var inicio = Number(iniciotxt.value)
-    var fim = Number(fimtxt.value)
-    var passo = Number(passotxt.value)
-
-    var contagem = window.document.getElementById('contagem')
-    var resultado = window.document.getElementById('resultado')
-
-    if (inicio >= fim) {
-        contagem.innerHTML = ""
-        resultado.innerHTML = "<strong>Impossível Contar!, O inicio está maior ou igual ao fim.</strong>"
-    } else if (passo > fim ) {
-        contagem.innerHTML = "Preparando Contagem..."
-        window.alert('[ERRO], passos maiores do que fim.')
-        resultado.innerHTML = ""
-    } else if (passo == 0) {
-        resultado.innerHTML = ""
-        contagem.innerHTML = "<strong>Impossível Contar!, Não é possível contar de 0 em 0</strong>"
+    if (formInicio.value.length === 0 || formFim.value.length === 0 || formPas.value.length === 0) {
+        res.innerHTML = `Impossível realizar a contagem!`
     } else {
-        resultado.innerHTML = ``
-        contagem.innerHTML = "Contando:"
-        
-        for (var c = inicio; c < fim; c += passo) {
-            resultado.innerHTML += `${c} 👉 `
+        res.innerHTML = 'Contando:<br>'
+        var i = Number(formInicio.value)
+        var f = Number(formFim.value)
+        var p = Number(formPas.value)
+        if (p <= 0) {
+            window.alert('Passo Inválido! Considerando Passo 1')
+            p = 1
         }
-
-        resultado.innerHTML += `🏁`
+        if (i < f) {
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449} ` 
+            }
+        } else { 
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
+    
 
 }
